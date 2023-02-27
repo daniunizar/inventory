8693fb2d474deb5a065dad080e77e9d203700d21
Daniel Marcos <daniunizar@gmail.com>
2023-02-27 20:48:41 +0100

    add boardgame create route

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,7 @@ Route::group([
   Route::get('boardgame/items/{user_id}', [App\Http\Controllers\Boardgame\GetBoardgameListController::class, '__invoke'])->name('boardgame.items');
   Route::get('boardgame/item/show/{boardgame_id}', [App\Http\Controllers\Boardgame\ShowBoardgameController::class, '__invoke'])->name('boardgame.item.show');
   Route::post('boardgame/item/store', [App\Http\Controllers\Boardgame\StoreBoardgameController::class, '__invoke'])->name('boardgame.item.store');
+  Route::post('boardgame/item/create', [App\Http\Controllers\Boardgame\StoreBoardgameController::class, '__invoke'])->name('boardgame.item.create');
   Route::put('boardgame/item/update', [App\Http\Controllers\Boardgame\UpdateBoardgameController::class, '__invoke'])->name('boardgame.item.update');
   Route::delete('boardgame/item/delete/{boardgame_id}', [App\Http\Controllers\Boardgame\DeleteBoardgameController::class, '__invoke'])->name('boardgame.item.delete');
   //auth
